e7e66c77c3537923b2e25a8f962114db02169b22
whzhni1 <whzhni@163.com>
2025-11-12 21:29:45 +0800

    Update release-GitCode.sh

--- .github/workflows/scripts/release-GitCode.sh
+++ .github/workflows/scripts/release-GitCode.sh
@@ -217,7 +217,7 @@ ${REPO_DESC}
         -d @-)
     
     # 检查是否成功
-    if echo "$response" | jq -e '.content.sha' > /dev/null 2>&1; then
+    if jq -e '.content.sha' || jq -e '.commit.sha' || jq -e '.sha'; then
         log_success "初始文件创建成功"
         return 0
     else
